bf63329f2cb339febb30c3f200f9e019f99dea1e
chong <admin@admins-MacBook-Pro.local>
2020-07-24 11:56:42 +0800

    init

---
+++ src/.umi/plugin-dva/exports.ts
@@ -0,0 +1,4 @@
+// @ts-nocheck
+
+export { connect } from '/Users/admin/code/docker-demo/node_modules/dva';
+export { getApp as getDvaApp } from './dva';
---
+++ src/.umi/umi.ts
@@ -0,0 +1,46 @@
+// @ts-nocheck
+import './core/polyfill';
+import '../global.tsx';
+import { plugin } from './core/plugin';
+import { createHistory } from './core/history';
+import { ApplyPluginsType } from '/Users/admin/code/docker-demo/node_modules/@umijs/runtime';
+import { renderClient } from '/Users/admin/code/docker-demo/node_modules/@umijs/renderer-react/dist/index.js';
+
+
+require('../global.less');
+require('./plugin-locale/locale')._onCreate();
+
+const getClientRender = (args: { hot?: boolean } = {}) => plugin.applyPlugins({
+  key: 'render',
+  type: ApplyPluginsType.compose,
+  initialValue: () => {
+    return renderClient({
+      // @ts-ignore
+      routes: require('./core/routes').routes,
+      plugin,
+      history: createHistory(args.hot),
+      isServer: process.env.__IS_SERVER,
+      dynamicImport: true,
+      rootElement: 'root',
+    });
+  },
+  args,
+});
+
+const clientRender = getClientRender();
+export default clientRender();
+
+
+    window.g_umi = {
+      version: '3.2.10',
+    };
+  
+
+// hot module replacement
+// @ts-ignore
+if (module.hot) {
+  // @ts-ignore
+  module.hot.accept('./core/routes', () => {
+    getClientRender({ hot: true })();
+  });
+}
